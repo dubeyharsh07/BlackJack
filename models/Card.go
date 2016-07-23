c47fd18180df879844aed16dae01512675cfb3d6
dubeyharsh07 <dubey.harsh07@gmail.com>
2016-07-23 15:50:52 +0530

    card test cases

--- models/Card.go
+++ models/Card.go
@@ -1,26 +1,29 @@
 package model
 
-import "fmt" 
-
-	
-var cardValues map[string][]int {
-	Ace : []{1,10}
-	Card2 : []{2}
-	Card3 : []{3}
-	Card4 : []{4}
-	Card5 : []{5}
-	Card6 : []{6}
-	Card7 : []{7}
-	Card8 : []{8}
-	Card9 : []{9}
-	Card10 : []{10}
-	Jack : []{10}
-	Queen : []{10}
-	King : []{10}
+var cardValues = map[string][]int{
+	"Ace" : []int{1,10},
+	"Card2" : []int{2},
+	"Card3" : []int{3},
+	"Card4" : []int{4},
+	"Card5" : []int{5},
+	"Card6" : []int{6},
+	"Card7" : []int{7},
+	"Card8" : []int{8},
+	"Card9" : []int{9},
+	"Card10" : []int{10},
+	"Jack" : []int{10},
+	"Queen" : []int{10},
+	"King" : []int{10},
 }
 
 type Card struct {
 	cardValue []int
 	cardType string
+}
+
+func newCard(t string) *Card {
+
+	newCard := Card{cardType:t, cardValue: cardValues[t]}
+	return &newCard
 }
 
--- models/Card_test.go
+++ models/Card_test.go
@@ -2,6 +2,13 @@ package model
 
 import "testing"
 
-func TestOneCard(t *testing.T) {
+func TestAceCard(t *testing.T) {
+
 	
+	card := newCard("Ace")
+
+	if card.cardValue[0] != 1 || card.cardValue[1] !=10 {
+		t.Errorf("Should have contained a card with value %d", 1)
+	}
+
 }
